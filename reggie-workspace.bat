efe3c444d4e604a39631e1d96bbfe30b1aed9495
blueivy828 <blueivy3142@gmail.com>
2025-12-26 08:21:21 +0000

    Update URL for grok and add new commands

--- reggie-workspace.bat
+++ reggie-workspace.bat
@@ -1,3 +1,6 @@
 @echo off
 
-start "" msedge "https://chat.openai.com" "https://claude.ai" "https://grok.x.ai" "https://www.youtube.com" "https://github.com" "https://www.google.com"
+start "" msedge "https://chat.openai.com" "https://claude.ai" "https://grok.com" "https://www.youtube.com" "https://github.com" "https://www.google.com"
+
+start "" powershell
+start "" "C:\Users\PC\AppData\Local\Programs\Obsidian\Obsidian.exe"
